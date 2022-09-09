7b79a5795a4ed628c8337805bcc7273a0704d373
Green <97376220+Nga2710@users.noreply.github.com>
2022-09-09 10:45:29 +0700

    Add files via upload

--- assets/js/json-formattor.js
+++ assets/js/json-formattor.js
@@ -6,113 +6,19 @@ function toggle_on() {
   toggle.removeEventListener("click", toggle_on);
   toggle.classList.add("toggle_on");
   toggle.classList.remove("toggle_off");
-  toggle1 = true;
+  toggle1 = true
 }
 function toggle_off() {
   toggle.addEventListener("click", toggle_on);
   toggle.removeEventListener("click", toggle_off);
   toggle.classList.add("toggle_off");
   toggle.classList.remove("toggle_on");
-  toggle1 = false;
+  toggle1 = false
 }
-function format() {
-  var newStr = "",
-  space = 0,
-  number = "0123456789",
-  str = document.getElementById("text").value.replace(/ /g, "").replace(/\t/g, "").replace(/\n/g, "").replace(/":/g, '": ');
-  for (let i = 0; i < str.length; i++) {
-    var char = str[i];
-    if (char == ',' && (str[i-1] == '"' || str[i-1] == "0") {
-      newStr += ",\n";
-      for (let n = space; n > 0; n = n-1) {
-        if (!toggle1) newStr += "  "; else newStr += "	";
-      }
-      continue;
-    }
-    if (char == '"' && (str[i+1] == '}' || str[i+1] == ']')) {
-      space -= 1;
-      newStr += '"\n';
-      for (let n = space; n > 0; n = n-1) {
-        if (!toggle1) newStr += "  "; else newStr += "	";
-      }
-      continue;
-    }
-    if (char == "{") {
-      if (str[i+1] == "}") {
-        newStr += "{}";
-        if (str[i+2] == ",") {
-          newStr += ",\n";
-          i += 2;
-        } else {
-          space -= 1;
-          newStr += "\n";
-          i++;
-        }
-        for (let n = space; n > 0; n = n-1) {
-          if (!toggle1) newStr += "  "; else newStr += "	";
-        }
-        continue;
-      }
-      space++;
-      newStr += "{\n";
-      for (let n = space; n > 0; n = n-1) {
-        if (!toggle1) newStr += "  "; else newStr += "	";
-      }
-      continue;
-    }
-    if (char == "[") {
-      if (str[i+1] == "]") {
-        newStr += "[]";
-        if (str[i+2] == ",") {
-          newStr += ",\n";
-          i += 2;
-        } else {
-          space -= 1;
-          newStr += "\n";
-          i++;
-        }
-        for (let n = space; n > 0; n = n-1) {
-          if (!toggle1) newStr += "  "; else newStr += "	";
-        }
-        continue;
-      }
-      space++;
-      if (number.includes(char)) {
-        newStr += "[";
-      } else {
-        newStr += "[\n";
-        for (let n = space; n > 0; n = n-1) {
-          if (!toggle1) newStr += "  "; else newStr += "	";
-        }
-      }
-      continue;
-    }
-    if (char == '}') {
-      if (str[i+1] == ',') {
-        newStr += "},\n"; i++
-      } else {
-        newStr += "}\n"; space -= 1;
-      }
-      for (let n = space; n > 0; n = n-1) {
-        if (!toggle1) newStr += "  "; else newStr += "	";
-      }
-      continue;
-    }
-    if (char == ']') {
-      if (str[i-1] != '"') {
-        newStr += "\n"
-      }
-      if (str[i+1] == ',') {
-        newStr += "],\n"; i++
-      } else {
-        newStr += "]\n"; space -= 1;
-      }
-      for (let n = space; n > 0; n = n-1) {
-        if (!toggle1) newStr += "  "; else newStr += "	";
-      }
-      continue;
-    }
-    newStr += char;
+function formatjson11(level) {
+  try {
+    text.value = `${JSON.stringify(JSON.parse(document.getElementById("text").value), null, level)}`
+  } catch (err) {
+    let width = "450px" let height = "100px" document.getElementById("text").innerHTML = `<div style="right:10px; bottom:10px;height: ${height};width: ${width};background: #a30000;position: absolute;padding-top: 20px;border-radius: 10px;vertical-align:top;box-shadow: inset 0 0 20px #330006;"> <div style="position: absolute; top: 0; right: 0">x&nbsp;</div> <div style="position: relative; left: 10px">${err}</div> </div>`
   }
-  text.value = newStr;
 }
