2feb6bd1821e25f74fe3f9c2803221c2fd6ffe2f
Green <97376220+Nga2710@users.noreply.github.com>
2023-04-12 21:52:04 +0700

    Add files via upload

--- assets/js/encode-decode.js
+++ assets/js/encode-decode.js
@@ -1,18 +1,18 @@
 var imgencode = document.getElementById("encode"),
 imgdecode = document.getElementById("decode"),
-str, resultStr, hex;
+str, resultStr, hex;/*
 imgencode.onclick = () => {
   str = document.querySelector("textarea").value;
   resultStr = '';
   let replace = false;
   for (let i = 0; i < str.length; i++) {
     var char = str[i];
-    // /*hsheh*/
+  
     if (char == "/" && replace == false && toggleb == true) {
       i++;
       if (str[i] == "*") {
         for (var n = i+1; n-1 < str.length; n++) {
-          if (str[n] == "*" && str[n+1] == "/" /*&& n+1 < str.length*/) break; else continue;
+          if (str[n] == "*" && str[n+1] == "/") break; else continue;
         }
         i = n+1;
         continue;
@@ -56,6 +56,36 @@ imgencode.onclick = () => {
     text.value = resultStr;
   }
 };
+Có thể viết gọn lại mã JavaScript trên như sau:
+
+```javascript*/
+imgencode.onclick = () => {
+  const str = document.querySelector("textarea").value;
+  let resultStr = '';
+  let replace = false;
+
+  for (const char of str) {
+    if (char === '"' && str[str.indexOf(char)-1] !== "\\") {
+      replace = !replace;
+    }
+    if (!replace && (!toggleb || !char.match(/\s/))) {
+      resultStr += char;
+    } else if (replace) {
+      resultStr += `\\u${('0000'+char.charCodeAt(0).toString(16)).substr(-4)}`;
+    }
+  }
+
+  if (text.value === resultStr) {
+    toastShow("Lỗi", "Mã của bạn không ở dạng hoàn chỉnh!", "w")
+  } else {
+    toastShow("Đã mã hóa thành công!", "", "s");
+    text.value = resultStr;
+  }
+};/*
+
+
+
+
 imgdecode.onclick = () => {
   str = document.querySelector("textarea").value;
   resultStr = '';
@@ -74,13 +104,31 @@ imgdecode.onclick = () => {
     }
     if (char == "\\" && str[i+1] == "u" && i+6 <= str.length) {
       hex = str[i+2] + str[i+3] + str[i+4] + str[i+5];
-      resultStr += String.fromCharCode(parseInt(hex/*.substr(n, 4)*/, 16));
+      resultStr += String.fromCharCode(parseInt(hex, 16));
       i += 5;
     } else resultStr += char;
   }
   if (text.value == resultStr) {
     toastShow("Lỗi", "Chưa có kí tự nào được thay đổi, kiểm tra và đảm bảo mã của bạn ở dạng \\u[hex][hex][hex][hex]", "w")
   } else {
+    text.value = resultStr;
+    toastShow("Đã giải mã thành công!", "", "s");
+  }
+};
+
+*/
+imgdecode.onclick = () => {
+  const str = document.querySelector("textarea").value;
+  let resultStr = "";
+  let replace = false;
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    if (char == '"' && str[i-1] != "\\") replace = !replace;
+    resultStr += !replace && char != "\\" ? char : char == "\\" && str[i+1] == "u" && i+6 <= str.length ? String.fromCharCode(parseInt(str.substr(i+2, 4), 16)) : char;
+    if (resultStr != str && char == "\\" && str[i+1] == "u" && i+6 <= str.length) i++;
+  }
+  if (resultStr == str) toastShow("Lỗi", "Chưa có kí tự nào được thay đổi, kiểm tra và đảm bảo mã của bạn ở dạng \\u[hex][hex][hex][hex]", "w");
+  else {
     text.value = resultStr;
     toastShow("Đã giải mã thành công!", "", "s");
   }
